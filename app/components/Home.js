af8aa2cdb4729e0808004bf00510ad719915249a
Fendy Heryanto <fendy3002@yahoo.com>
2017-12-25 22:30:22 +0700

    Change CSS and home page

--- app/components/Home.js
+++ app/components/Home.js
@@ -6,11 +6,8 @@ import styles from './Home.css';
 export default class Home extends Component {
   render() {
     return (
-      <div>
-        <div className={styles.container} data-tid="container">
-          <h2>Home</h2>
-          <Link to="/counter">to Counter</Link>
-        </div>
+      <div className="ui container">
+        <h1>Semantic UI</h1>  
       </div>
     );
   }
